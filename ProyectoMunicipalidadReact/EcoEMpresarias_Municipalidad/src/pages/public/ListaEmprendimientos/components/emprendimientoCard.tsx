418ffe5d95404991b2d8194157c25ffd78e94de7
sandfall74 <adrianpiedrasan@gmail.com>
2026-02-18 16:37:53 -0600

    1

--- ProyectoMunicipalidadReact/EcoEMpresarias_Municipalidad/src/pages/public/ListaEmprendimientos/components/emprendimientoCard.tsx
+++ ProyectoMunicipalidadReact/EcoEMpresarias_Municipalidad/src/pages/public/ListaEmprendimientos/components/emprendimientoCard.tsx
@@ -30,7 +30,7 @@ export function EmprendimientoCard({
         <div className="relative aspect-[16/10] overflow-hidden bg-muted">
           {emprendedor.ruta_Imagen_Logo ? (
             <img
-              src={`https://localhost:7050/api/Images/Buscar/${emprendedor.ruta_Imagen_Logo}`}
+              src={`https://localhost:7050/api/Images/Buscar/1/${emprendedor.ruta_Imagen_Logo}`}
               alt={emprendedor.nombre}
               className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
             />
