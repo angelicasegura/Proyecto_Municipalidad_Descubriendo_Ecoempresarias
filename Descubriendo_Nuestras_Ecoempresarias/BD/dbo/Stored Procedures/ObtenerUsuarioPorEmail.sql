9efb97be5d67427fae160b73c3d1cf0573f6220c
Alerex12 <alejandromuniz1218@gmail.com>
2026-07-09 22:42:06 -0600

    correcciones olvidó contraseña

--- Descubriendo_Nuestras_Ecoempresarias/BD/dbo/Stored Procedures/ObtenerUsuarioPorEmail.sql
+++ Descubriendo_Nuestras_Ecoempresarias/BD/dbo/Stored Procedures/ObtenerUsuarioPorEmail.sql
@@ -4,7 +4,7 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT [Usuario_id],
+    SELECT [Usuario_id] AS IdUsuario,
            [Nombre],
            [Apellidos],
            [Telefono],
@@ -12,8 +12,8 @@ BEGIN
            [Email],
            [Ruta_Imagen_Perfil],
            [Edad],
-           [Estado_id],
-           [Rol_id]
+           [Estado_id] AS IdEstado,
+           [Rol_id] AS IdRol
     FROM [dbo].[ECOEMPRESARIAS_USUARIOS_TB]
     WHERE Email = @Email;
 END
